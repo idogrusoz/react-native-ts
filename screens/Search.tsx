09df7d827c933247c069e97aca002ab41760e673
Ibrahim Doğrusöz <ibrahim.dogrusoz@sbf.finance>
2020-11-16 15:00:12 +0100

    initial commit

---
+++ screens/Search.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "../components/Themed";
+
+const Search = () => {
+    return <View></View>;
+};
+
+export default Search;
--- types.tsx
+++ types.tsx
@@ -1,17 +1,17 @@
 export type RootStackParamList = {
-  Root: undefined;
-  NotFound: undefined;
+    Root: undefined;
+    NotFound: undefined;
 };
 
 export type BottomTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
+    Search: undefined;
+    Random: undefined;
 };
 
 export type TabOneParamList = {
-  TabOneScreen: undefined;
+    SearchTab: undefined;
 };
 
 export type TabTwoParamList = {
-  TabTwoScreen: undefined;
+    RandomTab: undefined;
 };
