2e040de2945a180a76ba2f0c4c5d8826e31b0124
idogrusoz <idogrusoz@gmail.com>
2020-11-17 02:22:01 +0100

    FEAT: Navigate to results page

--- components/SelactedWrapper/SelectedWrapper.tsx
+++ components/SelactedWrapper/SelectedWrapper.tsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import { TouchableOpacity, StyleSheet } from "react-native";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Text } from "../Themed";
 import Selected from "../Selected/Selected";
 import { primary } from "../../constants/Colors";
 import { ingredientsSelector } from "../../redux/selectors";
+import { clearSelected, setSearchResults } from "../../redux/actions/actions";
+import { Ingredient } from "../../types";
+type SelectedWrapperProps = {
+    navigate: (route: string) => void;
+};
 
-const SelectedWrapper = () => {
+const SelectedWrapper: FunctionComponent<SelectedWrapperProps> = ({ navigate }) => {
     const { selected } = useSelector(ingredientsSelector);
+    const dispatch = useDispatch();
+    const handleSearch = () => {
+        dispatch(setSearchResults(selected.map((item: Ingredient) => item.name)));
+        navigate("SearchResults");
+        dispatch(clearSelected());
+    };
     return (
         <>
             {selected.map((item, i) => {
                 return <Selected name={item.name} index={i} key={item.id.toString()} />;
             })}
             {selected.length > 0 && (
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity onPress={handleSearch} style={styles.button}>
                     <Text>Search</Text>
                 </TouchableOpacity>
             )}
