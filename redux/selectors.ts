89512fd59574ab2d945dab55ac6dcd6650b5221b
idogrusoz <idogrusoz@gmail.com>
2020-11-17 02:23:09 +0100

    FEAT: Add recipes selector

--- redux/selectors.ts
+++ redux/selectors.ts
@@ -1,2 +1,3 @@
 import { RootState } from "./../types";
 export const ingredientsSelector = (state: RootState) => state.ingredients;
+export const recipesSelector = (state: RootState) => state.recipes;
